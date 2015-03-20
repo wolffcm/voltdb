f355d47cf485444d8d2a572095873a1e08076969
Christopher M. Wolff <cwolff@voltdb.com>
2015-03-20 12:57:45 -0400

    Update geospatial example for demo

--- examples/geospatial/ddl.sql
+++ examples/geospatial/ddl.sql
@@ -7,7 +7,6 @@ create table us_cities (
        population bigint not null,
        geo_json varchar(128) not null
 );
-partition table us_cities on column id;
 
 create table us_states (
        id integer not null primary key,
@@ -25,10 +24,37 @@ create table us_counties (
 
 -- radius of earth is 3959 miles
 
-create procedure areaOfState as
+create procedure largestStates as
        select name, geo_area(geo_json) * 3959*3959 as area_in_miles
        from us_states
-       where name = ?;
+       order by area_in_miles desc
+       limit 5;
+
+create procedure mostDistantCities as
+       select cities1.name,
+              cities2.name,
+              geo_distance(cities1.geo_json, cities2.geo_json)
+                * 3959 as distance
+       from us_cities as cities1, us_cities as cities2
+       where cities1.id < cities2.id
+       order by distance desc
+       limit 5;
+
+create procedure statesWithMostPolygons as
+       select us_states.name,
+              geo_num_polygons(us_states.geo_json) as num_polys
+       from us_states
+       order by num_polys desc
+       limit 5;
+
+create procedure statesWithFewestPoints as
+       select us_states.name,
+              geo_num_points(us_states.geo_json) as num_points
+       from us_states
+       order by num_points asc
+       limit 5;
+
+-- geo-spatial joins!
 
 create procedure citiesInState as
        select us_cities.name, us_cities.population
@@ -37,6 +63,24 @@ create procedure citiesInState as
          on geo_within(us_cities.geo_json, us_states.geo_json) = 1
        where us_states.name = ?
        order by us_cities.population desc;
+
+
+create procedure citiesPerCounty as
+       select us_counties.name as county_name,
+              count(*) as cnt
+       from us_counties
+         inner join us_states
+           on us_counties.state_id = us_states.id
+         inner join us_cities
+         on geo_within(us_cities.geo_json, us_counties.geo_json) = 1
+       where us_states.name = ?
+       group by county_name
+       order by cnt desc;
+
+
+
+----------------------------------------
+
 
 create procedure numCitiesPerState as
        select us_states.name, count(*)
@@ -54,4 +98,9 @@ create procedure populationPerState as
          on geo_within(us_cities.geo_json, us_states.geo_json) = 1
        group by us_states.name
        order by population desc;
+
+create procedure averagePointsPerState as
+       select avg(geo_num_points(us_states.geo_json)) as avg_pts
+       from us_states;
+
 EOF
