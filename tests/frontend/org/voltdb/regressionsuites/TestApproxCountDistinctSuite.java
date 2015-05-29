2aa27bb11f6e8004ca2fdbddb6c40663a56eb881
Chris Wolff <cwolff@voltdb.com>
2015-05-29 16:03:03 -0700

    Add distributed test case

--- tests/frontend/org/voltdb/regressionsuites/TestApproxCountDistinctSuite.java
+++ tests/frontend/org/voltdb/regressionsuites/TestApproxCountDistinctSuite.java
@@ -29,9 +29,25 @@ import java.util.Random;
 import org.voltdb.BackendTarget;
 import org.voltdb.VoltTable;
 import org.voltdb.client.Client;
+import org.voltdb.client.NoConnectionsException;
+import org.voltdb.client.ProcCallException;
 import org.voltdb.compiler.VoltProjectBuilder;
 
 public class TestApproxCountDistinctSuite extends RegressionSuite {
+
+    private static void fillTable(Client client, String tbl) throws NoConnectionsException, IOException, ProcCallException {
+        Random r = new Random(777);
+        for (int i = 0; i < 1000; ++i) {
+
+            double d;
+            do {
+                d = r.nextGaussian() * 1000;
+            } while (d > Long.MAX_VALUE || d <= Long.MIN_VALUE);
+
+            long val = (long) d;
+            client.callProcedure(tbl + ".Insert", i, val);
+        }
+    }
 
     public void testSimple() throws Exception
     {
@@ -43,20 +59,37 @@ public class TestApproxCountDistinctSuite extends RegressionSuite {
         assertEquals(0.0, vt.getDouble(0));
         assertFalse(vt.advanceRow());
 
-        Random r = new Random(777);
-        for (int i = 0; i < 1000; ++i) {
-            double d = r.nextGaussian();
-            while (d > Long.MAX_VALUE || d <= Long.MIN_VALUE) {
-                d = r.nextGaussian();
-            }
-            long val = (long) d;
-            client.callProcedure("@AdHoc", "insert into r values (" +  val + ")");
-        }
+        fillTable(client, "r");
 
         System.out.println("Finished filling table!");
 
         vt = client.callProcedure("@AdHoc",
                 "select approx_count_distinct(bi), count(distinct bi)  from r;")
+                .getResults()[0];
+        System.out.println(vt);
+    }
+
+    public void testDistributed() throws Exception {
+        Client client = getClient();
+
+        // This still sends all the rows over the network.
+        // Need to rework the planner to do this right.
+        VoltTable vt = client.callProcedure("@Explain", "select approx_count_distinct(bi) from p;")
+                .getResults()[0];
+        System.out.println(vt);
+
+        vt = client.callProcedure("@AdHoc", "select approx_count_distinct(bi) from p;")
+                .getResults()[0];
+        assertTrue(vt.advanceRow());
+        assertEquals(0.0, vt.getDouble(0));
+        assertFalse(vt.advanceRow());
+
+        fillTable(client, "p");
+
+        System.out.println("Finished filling table!");
+
+        vt = client.callProcedure("@AdHoc",
+                "select approx_count_distinct(bi), count(distinct bi)  from p;")
                 .getResults()[0];
         System.out.println(vt);
     }
@@ -72,9 +105,15 @@ public class TestApproxCountDistinctSuite extends RegressionSuite {
         VoltProjectBuilder project = new VoltProjectBuilder();
         final String literalSchema =
                 "CREATE TABLE r ( " +
+                        "pk integer primary key not null, " +
                         "bi bigint " +
-                        ");"
-                        ;
+                        ");" +
+                        "CREATE TABLE p ( " +
+                        "pk integer primary key not null, " +
+                        "bi bigint " +
+                        ");" +
+                        "partition table p on column pk;" +
+                        "";
         try {
             project.addLiteralSchema(literalSchema);
         } catch (IOException e) {
@@ -82,7 +121,7 @@ public class TestApproxCountDistinctSuite extends RegressionSuite {
         }
         boolean success;
 
-        config = new LocalCluster("testApproxCountDistinctSuite-onesite.jar", 1, 1, 0, BackendTarget.NATIVE_EE_JNI);
+        config = new LocalCluster("testApproxCountDistinctSuite-onesite.jar", 2, 1, 0, BackendTarget.NATIVE_EE_JNI);
         success = config.compile(project);
         assert(success);
         builder.addServerConfig(config);
